2b48e39bbb73e3b95ec195e99b97821e1c3a0557
primus2013 <359237542@qq.com>
2017-06-25 22:09:51 +0800

    补充漏掉的kind editor

---
+++ fl_kt/static/js/kindeditor-4.1.10/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by PRIMUS on 2017/6/25.
+ */
+KindEditor.ready(function(K) {
+                window.editor = K.create('textarea[name=bz]',
+                {width:500,
+                height:200});
+        });
