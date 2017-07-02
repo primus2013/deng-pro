fd9060c77f026f7aa9fb858bec627264de787674
primus2013 <359237542@qq.com>
2017-07-02 21:14:52 +0800

    找到原版博客模板可以使用

---
+++ fl_kt/Acc/static/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by PRIMUS on 2017/7/1.
+ */
--- fl_kt/Acc/views.py
+++ fl_kt/Acc/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.conf import settings
+from models import *
 
 import logging
+
 # Create your views here.
+logger=logging.getLogger('Acc.views')
+
+
+def global_setting(request):
+    return {'SITE_NAME': settings.SITE_NAME,
+            'SITE_DESC': settings.SITE_DESC,}
 
 
 def index(request):
+    try:
+        pass
+    except Exception as e:
+        logger.error(e)
     return render(request, 'index.html', locals())
